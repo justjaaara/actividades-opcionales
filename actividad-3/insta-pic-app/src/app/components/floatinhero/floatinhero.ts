938ae4a60dbab059292cdc1d87a0da5b02c27ae4
Felipe Villa <villajaramillofelipe4@gmail.com>
2025-09-02 13:09:12 -0500

    feat: Add user profile and gallery components with associated styles and images

---
+++ actividad-3/insta-pic-app/src/app/components/floatinhero/floatinhero.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floatinhero',
+  imports: [],
+  templateUrl: './floatinhero.html',
+  styleUrl: './floatinhero.css'
+})
+export class Floatinhero {
+
+}
--- actividad-3/insta-pic-app/src/app/features/pages/home/home.ts
+++ actividad-3/insta-pic-app/src/app/features/pages/home/home.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../../shared/components/navbar/navbar';
+import { Floatinhero } from '../../../components/floatinhero/floatinhero';
+import { Gallery } from '../../../components/gallery/gallery';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [Navbar, Floatinhero, Gallery],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
